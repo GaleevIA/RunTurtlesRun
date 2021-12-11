95a6a8590c6d35f8dd97f018cd7666b6d6048bd1
Iliyas Galeev <galeev.iliyas@mail.ru>
2021-12-11 11:35:47 +0300

    4
    4

---
+++ Task1/TestovoeZadanie_Task1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace TestovoeZadanie_Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String[] strings = new String[]{ "din", "recede", "Success", "(( @" };
+            String[] strings2 = new String[strings.Length];
+
+            for(int i = 0; i < strings.Length; i++)
+            {
+                strings2[i] = String.Concat(strings[i].ToLower().Select(x => strings[i].Where(y => x == y).Count() == 1 ? "(" : ")"));
+            }
+
+            for (int i = 0; i < strings.Length; i++)
+                Console.WriteLine($"{strings[i]} => {strings2[i]}");
+            
+            Console.ReadLine();
+        }
+    }
+}
